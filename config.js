1d4792ec8e782c7f1ef3d4c9febca3d4bc33a6c9
SyedMuhammedAbbas <66178330+SyedMuhammedAbbas@users.noreply.github.com>
2023-07-17 19:13:50 +0500

    code updated

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const BASEURL = "http://localhost:5000";
-// export const BASEURL = "https://careerify-backend.vercel.app";
+// export const BASEURL = "http://localhost:5000";
+export const BASEURL = "https://careerify-backend.vercel.app";
