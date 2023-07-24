05baf34d186b297910fa485a9f363167e1246ab1
SyedMuhammedAbbas <66178330+SyedMuhammedAbbas@users.noreply.github.com>
2023-07-24 06:39:27 +0500

    code updated

---
+++ src/components/Common/Graphs/PredictionGraphs/ProbOfTitleByCompany.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { BASEURL } from "../../../../../config";
+import axios from "axios";
+
+const ProbOfTitleByCompany = ({ title }) => {
+  const [jobData, setJobData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${BASEURL}/predict/ProbOfTitleByCompany?jobTitle=${title}`
+      );
+      const apiData = response.data;
+      console.log(apiData, "apiData");
+      setJobData(apiData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [title]);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4">
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={jobData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Company_name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="Probablity"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <div className="flex justify-center text-[15px] text-gray-500">
+        This graph represents recruitment probabilities based on {title} roles
+        for different companies. The data is generated using an LSTM AI model,
+        which analyzes historical hiring patterns to predict the likelihood of
+        recruiting for particular roles within each company. This visualization
+        offers insights into hiring preferences and enables informed recruitment
+        decisions.
+      </div>
+    </div>
+  );
+};
+
+export default ProbOfTitleByCompany;
--- src/components/Projection/ProjectionCharts.js
+++ src/components/Projection/ProjectionCharts.js
@@ -1,32 +1,15 @@
-import React, { useEffect, useRef } from "react";
-import CardBarChart from "../Cards/CardBarChart";
-import CardLineChart from "../Cards/CardLineChart";
-import CardPageVisits from "../Cards/CardPageVisits";
-import CardPieChart from "../Cards/CardPieChart";
-import CardSocialTraffic from "../Cards/CardSocialTraffic";
-import { BiSearch } from "react-icons/bi";
-import { SiBmcsoftware } from "react-icons/si";
-import CardLineChart2 from "../Cards/CardLineChart2";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { IoIosArrowForward } from "react-icons/io";
+import ProbOfTitleByCompany from "../Common/Graphs/PredictionGraphs/ProbOfTitleByCompany";
 
 export default function ProjectionCharts() {
-  var settings = {
-    dots: true,
-    // dotsClass: "slick-dots",
-    infinite: true,
-    speed: 400,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // slidesToShow: 4,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    // appendDots: (dots) => <ul>{dots}</ul>,
-    // customPaging: (i) => <div className="ft-slick__dots--custom"></div>,
-  };
+  const jobTitles = [
+    ".Net Developer",
+    "Content_writer",
+    "Data Scientist",
+    "Graphic Designer",
+    "React Developer",
+    "Wordpress Developer",
+  ];
 
   return (
     <>
@@ -39,64 +22,16 @@ export default function ProjectionCharts() {
             </div>{" "}
             <div>Projection</div>
           </div>
-          <div className="justify-end absolute right-10">
-            <form className="bg-[#e5e5e5] flex items-center rounded-[20px] w-[300px] h-[40px]">
-              <input
-                className="bg-[#e5e5e5] rounded-[20px] outline-none w-[250px] h-[30px] placeholder:text-[20px] placeholder:text-[#6a6a6a] pl-6 text-[25px] text-[#000000]"
-                placeholder="Search..."
-              />
-              <button className="rounded-[20px] flex justify-center text-[#6a6a6a] text-[30px] px-2">
-                <BiSearch />
-              </button>
-            </form>
-          </div>
         </div>
 
-        <div className=" grid gap-5 mx-auto justify-center overflow-y-auto overflow-x-hidden">
-          <div className="w-[70vw] mx-auto h-auto flex gap-4 justify-center my-5">
-            {/* <Slider {...settings}> */}
-            {[...Array(4)].map((i) => {
-              return (
-                <div className="w-[200px] bg-transparent">
-                  <div className="w-[150px] rounded-[10px] relative h-[149px] bg-[#474747] bg-opacity-30 z-10 shadow-md">
-                    <div className="w-[180px] rounded-[10px] absolute left-[-15px]  z-20 h-[140px] bg-[#ffffff] shadow-md">
-                      <div className="grid items-center p-3">
-                        <div className="text-[30px] text-[#0B9588]">
-                          <SiBmcsoftware />
-                        </div>
-                        <div className="text-[40px] font-semibold text-[#000000]">
-                          4.17
-                        </div>
-                        <div className="text-[15px] font-[400] text-[#7e7e7e]">
-                          Systems
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-            {/* </Slider> */}
-          </div>
-          <div className=" flex flex-wrap">
-            <div className=" w-3/4 mb-12 px-4">
-              <CardLineChart />
-            </div>
-            <div className=" w-1/4 px-4">
-              <CardBarChart />
-            </div>
-            <div className=" w-1/4 px-4">
-              <CardPieChart />
-            </div>
-            {/* <div className=" w-3/4 mb-12 px-4">
-              <CardLineChart2 />
-            </div> */}
-          </div>
-          {/* <div className="flex flex-wrap mt-4">
-            <div className="w-full xl:w-4/12 px-4">
-              <CardSocialTraffic />
-            </div>
-          </div> */}
+        <div className=" flex flex-wrap space-y-4">
+          {jobTitles.map((item, index) => {
+            return (
+              <div key={index} className=" w-2/4 xl3:w-[100%]  px-4">
+                <ProbOfTitleByCompany title={item} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
